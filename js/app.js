ac1d8acf5c2e4a94bbc193a8f3e38b8c19acbee1
userMauro-code <m88642336@gmail.com>
2025-09-08 23:13:15 -0300

    commit js inicialized

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(".card__submit");
+const ratingOptions = document.querySelectorAll('input[name="rating"]');
+const cardThank = document.querySelector(".card__thankYou");
+const scoreSpan = document.querySelector(".score");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let select = null;
+
+    ratingOptions.forEach((input)=>{
+        if(input.checked === true){
+            select = input.value;
+        } 
+    });
+    if(select) {
+        console.log(select);
+    }else{
+        alert("elige por favor");
+    }
+
+});   
